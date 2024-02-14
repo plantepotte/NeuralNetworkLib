bfbc9b9a7ed728f3edd15ffe3d73ad2eaec462a7
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2024-02-14 13:43:49 +0100

    Moved static functions to own lib

---
+++ NeuralNetworkLib/NeuralNetworkLib/ActivationLib.cpp
@@ -0,0 +1,27 @@
+﻿#include "ActivationLib.h"
+
+#include <stdexcept>
+
+double ActivationFunction(const double x, const EActivationFunction activationFunction) {
+    switch (activationFunction) {
+    case HEAVISIDE_STEP_FUNCTION:
+        return HeavisideStepFunction(x);
+    case SIGMOID_FUNCTION:
+        return SigmoidFunction(x);
+    case HYPERBOLIC_TANGENT_FUNCTION:
+        return HyperbolicTangentFunction(x);
+    }
+    throw std::invalid_argument("Invalid activation function");
+}
+
+double ActivationFunctionDerivative(const double x, const EActivationFunction activationFunction) {
+    switch (activationFunction) {
+    case HEAVISIDE_STEP_FUNCTION:
+        return HeavisideStepFunctionDerivative(x);
+    case SIGMOID_FUNCTION:
+        return SigmoidFunctionDerivative(x);
+    case HYPERBOLIC_TANGENT_FUNCTION:
+        return HyperbolicTangentFunctionDerivative(x);
+    }
+    throw std::invalid_argument("Invalid activation function");
+}
---
+++ NeuralNetworkLib/NeuralNetworkLib/ActivationLib.h
@@ -0,0 +1,41 @@
+﻿// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////
+// //FileName: ActivationLib.h
+// //FileType: Visual C++ Source file
+// //Author : Anders P. Åsbø
+// //Created On : 14/2/2024
+// //Last Modified On : 14/2/2024
+// //Description :
+// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////
+
+#pragma once
+#include <cmath>
+
+enum EActivationFunction : int {
+    HEAVISIDE_STEP_FUNCTION,
+    SIGMOID_FUNCTION,
+    HYPERBOLIC_TANGENT_FUNCTION
+};
+
+double ActivationFunction(double x, EActivationFunction activationFunction);
+
+double ActivationFunctionDerivative(double x, EActivationFunction activationFunction);
+
+inline double HeavisideStepFunction(const double x) { return x > 0.0 ? 1.0 : 0.0; }
+
+inline double SigmoidFunction(const double x) { return 1.0 / (1.0 + std::exp(-x)); }
+
+inline double HyperbolicTangentFunction(const double x) { return std::tanh(x); }
+
+inline double HeavisideStepFunctionDerivative(const double x) { return 0.0; }
+
+inline double SigmoidFunctionDerivative(const double x) {
+    const auto sigmoidOfX = SigmoidFunction(x);
+    return sigmoidOfX * (1.0 - sigmoidOfX);
+}
+
+inline double HyperbolicTangentFunctionDerivative(const double x) {
+    const auto hyperbolicTangentOfX = HyperbolicTangentFunction(x);
+    return 1.0 - hyperbolicTangentOfX * hyperbolicTangentOfX;
+}
