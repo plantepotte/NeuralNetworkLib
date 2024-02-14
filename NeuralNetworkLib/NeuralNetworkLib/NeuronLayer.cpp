1238cc87044e6be910145e4f3245c9f98550653d
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2024-02-14 13:44:41 +0100

    using eigen

--- NeuralNetworkLib/NeuralNetworkLib/NeuronLayer.cpp
+++ NeuralNetworkLib/NeuralNetworkLib/NeuronLayer.cpp
@@ -1,7 +1,27 @@
 ﻿#include "NeuronLayer.h"
 
-#include "Neuron.h"
+#include "ActivationLib.h"
+
+void NeuronLayer::CalcOutputs() {
+    // perform the dot product of the weights and inputs and add the biases
+    _outputs = _weights * _inputs + _biases;
+
+    // Apply the activation function to each output
+    for (double& output : _outputs) {
+        output = ActivationFunction(output, activationFunction);
+    }
+}
 
 NeuronLayer::NeuronLayer(int numberOfNeurons, int numberOfNeuronInputs): numNeurons(numberOfNeurons), numNeuronInputs(numberOfNeuronInputs) {
-    neurons = std::vector<Neuron>(numberOfNeurons, Neuron());
+
+    // Initialize the outputs, weights and biases with zeros, random values and random values, respectively
+    _outputs = Eigen::VectorXd::Zero(numNeurons);
+    _weights = Eigen::MatrixXd::Random(numNeurons, numNeuronInputs);
+    _biases = Eigen::VectorXd::Random(numNeurons);
+}
+
+Eigen::Vector<double, Eigen::Dynamic> NeuronLayer::CalcOutputs(const Eigen::Vector<double, Eigen::Dynamic>& inputs) {
+    _inputs = inputs;
+    CalcOutputs();
+    return _outputs;
 }
--- NeuralNetworkLib/NeuralNetworkLib/NeuronLayer.h
+++ NeuralNetworkLib/NeuralNetworkLib/NeuronLayer.h
@@ -10,20 +10,30 @@
 // //////////////////////////////
 
 #pragma once
-#include <vector>
+#include "Eigen/Eigen"
 
+enum EActivationFunction : int;
 class Neuron;
 
 class NeuronLayer {
 private:
+    Eigen::Vector<double, Eigen::Dynamic> _outputs{}; // Holds the outputs of each neuron in this layer
+    Eigen::Vector<double, Eigen::Dynamic> _inputs{}; // Holds the inputs to each neuron in this layer
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> _weights{}; // Holds the weights of each neuron in this layer
+    Eigen::Vector<double, Eigen::Dynamic> _biases{}; // Holds the biases of each neuron in this layer
+
+    void CalcOutputs();
 
 public:
     int numNeurons{}; // Holds the number of neurons in this layer
     int numNeuronInputs{}; // Holds the number of inputs to each neuron
+    EActivationFunction activationFunction{}; // Which activation function to use
 
-    std::vector<Neuron> neurons{};
 
     NeuronLayer() = default;
     NeuronLayer(int numberOfNeurons, int numberOfNeuronInputs);
-    
+
+    Eigen::Vector<double, Eigen::Dynamic> GetOutputs() const { return _outputs; }
+
+    Eigen::Vector<double, Eigen::Dynamic> CalcOutputs(const Eigen::Vector<double, Eigen::Dynamic>& inputs);
 };
